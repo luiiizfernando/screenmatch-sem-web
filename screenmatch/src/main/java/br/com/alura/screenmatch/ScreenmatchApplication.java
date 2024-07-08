80c83995b2d891b2aa1929045e3605a905888019
Luiz Fernando <luizfs138@gmail.com>
2024-07-08 17:30:23 -0300

    Concluído a segunda parte do curso spring

--- screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,17 @@
 package br.com.alura.screenmatch;
 
 import br.com.alura.screenmatch.model.DadoSerie;
+import br.com.alura.screenmatch.model.DadosEpisodio;
+import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.principal.Principal;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner
@@ -17,14 +23,9 @@ public class ScreenmatchApplication implements CommandLineRunner
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=4f486c5");
-        //System.out.println(json);
-        //json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-        System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadoSerie dados = conversor.obterDados(json, DadoSerie.class);
-		System.out.println(dados);
+
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 }
